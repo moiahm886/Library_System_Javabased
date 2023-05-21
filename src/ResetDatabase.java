edd5ee5b86262f27b432214b8c9b4b68d4eb47de
moiahm886 <moizahmedbss786@gmail.com>
2023-05-21 15:29:01 +0500

    DAO

--- src/ResetDatabase.java
+++ src/ResetDatabase.java
@@ -43,74 +43,23 @@ public class ResetDatabase extends JFrame {
         panel.add(resetIssuedBooks,constraints);
 
         add(panel);
-
+        ResetDatabaseDAO Reset = new ResetDatabaseDAO();
         resetViewBook.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try {
-                    Class.forName("com.mysql.cj.jdbc.Driver");
-                    String jdbcUrl = "jdbc:mysql://localhost:3306/books";
-                    String username = "root";
-                    String password = "";
-
-                    try (Connection conn = DriverManager.getConnection(jdbcUrl, username, password)) {
-                        String SQL = "TRUNCATE TABLE viewbooks";
-                        try (PreparedStatement statement = conn.prepareStatement(SQL)) {
-                            statement.executeUpdate();
-                        }
-                        System.out.println("Table viewbooks reset successfully.");
-                    } catch (SQLException ex) {
-                        throw new RuntimeException("Failed to execute SQL statement", ex);
-                    }
-                } catch (ClassNotFoundException ex) {
-                    throw new RuntimeException("MySQL JDBC Driver not found", ex);
-                }
+                Reset.resetBooks();
             }
         });
         resetViewUser.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try {
-                    Class.forName("com.mysql.cj.jdbc.Driver");
-                    String jdbcUrl = "jdbc:mysql://localhost:3306/books";
-                    String username = "root";
-                    String password = "";
-
-                    try (Connection conn = DriverManager.getConnection(jdbcUrl, username, password)) {
-                        String SQL = "TRUNCATE TABLE users";
-                        try (PreparedStatement statement = conn.prepareStatement(SQL)) {
-                            statement.executeUpdate();
-                        }
-                        System.out.println("Table users reset successfully.");
-                    } catch (SQLException ex) {
-                        throw new RuntimeException("Failed to execute SQL statement", ex);
-                    }
-                } catch (ClassNotFoundException ex) {
-                    throw new RuntimeException("MySQL JDBC Driver not found", ex);
-                }
+                Reset.resetUsers();
             }
         });
         resetIssuedBooks.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try {
-                    Class.forName("com.mysql.cj.jdbc.Driver");
-                    String jdbcUrl = "jdbc:mysql://localhost:3306/books";
-                    String username = "root";
-                    String password = "";
-
-                    try (Connection conn = DriverManager.getConnection(jdbcUrl, username, password)) {
-                        String SQL = "TRUNCATE TABLE issuedbooks";
-                        try (PreparedStatement statement = conn.prepareStatement(SQL)) {
-                            statement.executeUpdate();
-                        }
-                        System.out.println("Table users reset successfully.");
-                    } catch (SQLException ex) {
-                        throw new RuntimeException("Failed to execute SQL statement", ex);
-                    }
-                } catch (ClassNotFoundException ex) {
-                    throw new RuntimeException("MySQL JDBC Driver not found", ex);
-                }
+                Reset.issued();
             }
         });
 
---
+++ src/ResetDatabaseDAO.java
@@ -0,0 +1,77 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+interface resetDatabase{
+    void resetBooks();
+    void resetUsers();
+    void issued();
+}
+public class ResetDatabaseDAO implements resetDatabase {
+    @Override
+    public void resetBooks() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String jdbcUrl = "jdbc:mysql://localhost:3306/books";
+            String username = "root";
+            String password = "";
+
+            try (Connection conn = DriverManager.getConnection(jdbcUrl, username, password)) {
+                String SQL = "TRUNCATE TABLE viewbooks";
+                try (PreparedStatement statement = conn.prepareStatement(SQL)) {
+                    statement.executeUpdate();
+                }
+                System.out.println("Table viewbooks reset successfully.");
+            } catch (SQLException ex) {
+                throw new RuntimeException("Failed to execute SQL statement", ex);
+            }
+        } catch (ClassNotFoundException ex) {
+            throw new RuntimeException("MySQL JDBC Driver not found", ex);
+        }
+    }
+
+    @Override
+    public void resetUsers() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String jdbcUrl = "jdbc:mysql://localhost:3306/books";
+            String username = "root";
+            String password = "";
+
+            try (Connection conn = DriverManager.getConnection(jdbcUrl, username, password)) {
+                String SQL = "TRUNCATE TABLE users";
+                try (PreparedStatement statement = conn.prepareStatement(SQL)) {
+                    statement.executeUpdate();
+                }
+                System.out.println("Table users reset successfully.");
+            } catch (SQLException ex) {
+                throw new RuntimeException("Failed to execute SQL statement", ex);
+            }
+        } catch (ClassNotFoundException ex) {
+            throw new RuntimeException("MySQL JDBC Driver not found", ex);
+        }
+    }
+
+    @Override
+    public void issued() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String jdbcUrl = "jdbc:mysql://localhost:3306/books";
+            String username = "root";
+            String password = "";
+
+            try (Connection conn = DriverManager.getConnection(jdbcUrl, username, password)) {
+                String SQL = "TRUNCATE TABLE issuedbooks";
+                try (PreparedStatement statement = conn.prepareStatement(SQL)) {
+                    statement.executeUpdate();
+                }
+                System.out.println("Table users reset successfully.");
+            } catch (SQLException ex) {
+                throw new RuntimeException("Failed to execute SQL statement", ex);
+            }
+        } catch (ClassNotFoundException ex) {
+            throw new RuntimeException("MySQL JDBC Driver not found", ex);
+        }
+    }
+}
